06a0a1b39458197b27c32a2d08171bb971e51105
Andrea Buitrago <ac.buitrago403@uniandes.edu.co>
2018-05-02 14:36:12 -0500

    Final changes

--- torre-bio-app/src/app/profile/main.component.ts
+++ torre-bio-app/src/app/profile/main.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import { ProfileComponent, DialogAskUser } from './profile.component';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-main',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  error:string;
+
+  constructor(public dialog: MatDialog,private router: Router) { }
 
   ngOnInit() {
   }
+
+  openDialog(){
+    let dialogRef = this.dialog.open(DialogAskUser, {
+       width: '250px',
+       data: { error: this.error}
+     });
+
+     dialogRef.afterClosed().subscribe(result => {
+       if(result){
+         this.router.navigate([result]);
+
+       }else{
+         this.error= "Invalid profileid";
+         this.openDialog();
+       }
+     });
+  }
+
 
 }
